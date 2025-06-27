1c7d1a8bca425309d27a0a2647df65be5ec94d7e
irsol <irsol@users.noreply.github.com>
2025-06-27 11:38:06 +0200

    feat: find median in sorted arrays task

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 | 1 | Two sum             | [Link](./problems/0001-add-two-sum/solution.ts)     | Easy |
 | 2 | Add Two Numbers     | [Link](./problems/0002-add-two-numbers/solution.ts) | Medium |
 | 3 | Longest substring   | [Link](./problems/0003-longest-substring-unique-characters/solution.ts) | Medium |
+| 4 | Median of two sorted arrays | [Link](./problems/0004-median-of-two-sorted-arrays/solution.ts) | Hard |
+
 | 9 | Palindrome number   | [Link](./problems/0009-palindrom-number/solution.ts)| Easy |
---
+++ problems/0004-median-of-two-sorted-arrays/solution.ts
@@ -0,0 +1,35 @@
+export function findMedianSortedArraysSimple(nums1: number[], nums2: number[]): number {
+    const mergedArrays: number[] = [];
+    let i = 0, j = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] <= nums2[j]) {
+            mergedArrays.push(nums1[i]);
+            i++;
+        } else {
+            mergedArrays.push(nums2[j]);
+            j++;
+        };
+    };
+
+    // Handle cases when any remaining nums left in the arrays
+    while (i < nums1.length) {
+        mergedArrays.push(nums1[i]);
+        i++;
+    };
+
+    while (j < nums2.length) {
+        mergedArrays.push(nums2[j]);
+        j++;
+    };
+
+    // Find median
+    const totalLength = mergedArrays.length;
+    if (totalLength % 2 === 0) {
+        const mid1 = mergedArrays[totalLength / 2 - 1];
+        const mid2 = mergedArrays[totalLength / 2];
+        return (mid1 + mid2) / 2;
+    } else {
+        return mergedArrays[Math.floor(totalLength / 2)];
+    };
+};
